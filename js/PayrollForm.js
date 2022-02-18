52b764ca1bd3102969e67eb798f2622549c444f1
shaikmohammed238 <shaikmohammedghoue238@gmail.com>
2022-02-18 11:39:35 +0530

    addeduc3

--- js/PayrollForm.js
+++ js/PayrollForm.js
@@ -26,5 +26,68 @@ window.addEventListener('DOMContentLoaded', (event) => {
     });
 
 });
+/* UC3:- Ability to create Employee Payroll Object On Save. 
+         - Validation of Name and Date and if failed then set the UI accordingly. 
+*/
+const save = () => {
+    try {
+        let employeePayrollData = createEmployeePayroll();
+        createAndUpdateStorage(employeePayrollData);
+    }
+    catch (e) {
+        return;
+    }
+}
+
+const createEmployeePayroll = () => {
+    let employeePayrollData = new EmployeePayrollData();
+    try {
+        employeePayrollData.name = getInputValueById('#name');
+    }
+    catch (e) {
+        setTextValue('.text-error', e);
+        throw e;
+    }
+
+    employeePayrollData.profilePic = getSelectedValues('[name=profile]').pop();
+    employeePayrollData.gender = getSelectedValues('[name=gender]').pop();
+    employeePayrollData.department = getSelectedValues('[name=department]');
+    employeePayrollData.salary = getInputValueById('#salary');
+    employeePayrollData.note = getInputValueById('#notes');
+    //let date = getInputValueById('#day') + " " + getInputValueById('#month') + " " + getInputValueById('#year');
+    employeePayrollData.date = Date.parse(date);
+    alert(employeePayrollData.toString());
+    return employeePayrollData;
+}
+
+const getSelectedValues = (propertyValue) => {
+    let allItems = document.querySelectorAll(propertyValue);
+    let selItems = [];
+    allItems.forEach(item => {
+        if (item.checked)
+            selItems.push(item.value);
+    });
+    return selItems;
+}
+
+/*
+1: querySelector is the newer feature.
+2: The querySelector method can be used when selecting by element name, nesting, or class name.
+3: querySelector lets you find elements with rules that can't be expressed with getElementById
+*/
+const getInputValueById = (id) => {
+    let value = document.querySelector(id).value;
+    return value;
+}
+
+/*
+1: getElementById is better supported than querySelector in older versions of the browsers.
+2: The thing with getElementById is that it only allows to select an element by its id.
+*/
+const getInputElementValue = (id) => {
+    let value = document.getElementById(id).value;
+    return value;
+}
+
 
 
